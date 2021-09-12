e9e06f87cc88fdfca7b315f25b10f727f4948c81
SK <limsweekiat@gmail.com>
2021-09-12 20:21:25 +0800

    Fix copying of code

--- src/components/Gradient.vue
+++ src/components/Gradient.vue
@@ -157,7 +157,8 @@ export default {
     handleCode() {
       const text = document.querySelector(".gradient-css-code").textContent
       console.log(text)
-      document.execCommand("copy");
+      // document.execCommand("copy");
+      this.copyToClipboard(text)
     },
     hexToHSL(H) {
       // Convert hex to RGB first
@@ -248,6 +249,29 @@ export default {
         b = "0" + b;
 
       return "#" + r + g + b;
+    },
+    copyToClipboard (textToCopy) {
+      // navigator clipboard api needs a secure context (https)
+      if (navigator.clipboard && window.isSecureContext) {
+        // navigator clipboard api method'
+        return navigator.clipboard.writeText(textToCopy)
+      } else {
+        // text area method
+        let textArea = document.createElement("textarea")
+        textArea.value = textToCopy
+        // make the textarea out of viewport
+        textArea.style.position = "fixed"
+        textArea.style.left = "-999999px"
+        textArea.style.top = "-999999px"
+        document.body.appendChild(textArea)
+        textArea.focus()
+        textArea.select()
+        return new Promise((res, rej) => {
+          // here the magic happens
+          document.execCommand('copy') ? res() : rej()
+          textArea.remove()
+        })
+      }
     }
   },
 }
