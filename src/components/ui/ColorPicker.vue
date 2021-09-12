ff26396c25b446e530b7570bd48f1c02a2281b6d
SK <limsweekiat@gmail.com>
2021-09-12 13:39:22 +0800

    Update colorpicker UI

--- src/components/ui/ColorPicker.vue
+++ src/components/ui/ColorPicker.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="input">
+<div class="input" style="display: flex; justify-content: space-between;">
   <v-menu v-model="menu" top :close-on-content-click="false">
     <template v-slot:activator="{ on }">
       <v-btn
@@ -7,13 +7,19 @@
         v-bind="attrs"
         v-on="on"
         :change="$emit('change-color', color)"
+        style="flex-grow: 1; text-align: left;"
       >
         {{ color }}
       </v-btn>
       
       <div :style="swatchStyle" v-on="on" />
     </template>
-    <v-color-picker v-model="color" flat mode="hexa" />
+    <v-color-picker
+      v-model="color"
+      flat mode="hexa"
+      width="200"
+      dark="true"
+      />
   </v-menu>
 </div>
 </template>
@@ -45,6 +51,9 @@ export default {
         height: '20px',
         width: '20px',
         borderRadius: '50%',
+        border: '2px solid white',
+        boxShadow: '0px 0px 2px #111',
+        marginLeft: '10px',
       }
     }
   },
@@ -54,7 +63,7 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 .input {
   display: flex;
   align-items: center;
@@ -74,5 +83,19 @@ export default {
     border: 2px solid #2D2D2D;
     background-color: white;
   }
+}
+
+div.v-menu__content {
+  border-radius: 10px;
+  border: 3px solid #2d2d2d;
+}
+
+div.v-color-picker__canvas-dot {
+  border: 1px solid white;
+  box-shadow: 0px 0px 1px #111;
+}
+
+span.v-btn__content {
+  display: block;
 }
 </style>
